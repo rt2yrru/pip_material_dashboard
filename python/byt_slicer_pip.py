f14048bc741310483d10da03124298d28051ec30
siddht4 <dhar.75.ap@gmail.com>
2018-04-16 11:30:03 +0530

    Create byt_slicer_pip.py

---
+++ python/byt_slicer_pip.py
@@ -0,0 +1,12 @@
+import subprocess
+def pip_byt_slicer():
+	cmd1='pip list --format json'
+	r1 = subprocess.check_output(cmd1, shell=True)
+	print(type(r1))
+	n=r1.index(b'}]')+2
+	f1 = open('pip_list.json', "wb")
+	f1.write(r1[0:n])
+	f1.close()
+	f2=open('pip_list_2.txt','wb')
+	f2.write(r1[n+1:len(r1)])
+pip_byt_slicer()
